c12ae7e24ee6990160409e099c378d9d456c2bdb
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-30 19:28:06 +0300

    Initial Commit

---
+++ TypealiasSTBC/TypealiasSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypealiasSTBCApp.swift
+//  TypealiasSTBC
+//
+//  Created by Vahtee Boo on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TypealiasSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
